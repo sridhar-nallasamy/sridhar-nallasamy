722c33d79c3b57ce81c6c034c7b17558789de746
Sridhar Nallasamy <nsridharbtech@gmail.com>
2023-09-30 19:28:53 +0530

    hobby correction + animation added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -27,7 +27,7 @@ function TimelineList(props: TimelineProps) {
   const { emoji, year, where, descriptions } = props;
 
   return (
-    <li className="relative border-l-[1.5px] border-green-600 pl-6 pb-6 last:border-0 last:pb-0 before:absolute before:top-0 before:left-[-10px] before:w-4 before:h-4 before:bg-green-100 before:rounded-[50%] before:border-[1px] before:border-green-600 before:shadow-[2px_1px_0_rgb(22,163,74)]">
+    <li className="relative border-l-[1.5px] border-green-600 pl-6 pb-6 last:border-0 last:pb-0 before:absolute before:top-0 before:left-[-10px] before:w-4 before:h-4 before:bg-green-100 before:rounded-[50%] before:border-[1px] before:border-green-600 before:shadow-[2px_1px_0_rgb(22,163,74)] before:last:animate-spin">
       <h3 className="mb-1 text-base text-green-100">
         <span role="img" className="mr-2">
           {emoji}
@@ -87,7 +87,8 @@ export default function About() {
     },
     {
       prefixEmoji: '🚘 🏍️',
-      descriiption: 'Roaming around with friends, always feels home & peace',
+      descriiption:
+        'Roaming around with friends, and this always feels home & peace',
       suffixEmoji: '💖',
     },
   ];
