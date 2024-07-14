54a06ca62b70d24585966c53836ed2b83cf7eeae
Sridhar Nallasamy <nsridharbtech@gmail.com>
2024-07-15 00:38:29 +0530

    update 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{ts,tsx}',
+    './assets/svg/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
