2bdc2bb530e612f23b27a71b8df0bea414469af7
Sridhar Nallasamy <nsridharbtech@gmail.com>
2024-07-24 15:14:07 +0530

    Og Update & Scrollbar Stylings (#12)

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,30 @@ import './globals.css';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+const metaInfo = {
   title: 'Sridhar Nallasamy 😊',
   description: 'Portfolio Website of Sridhar Nallasamy',
+};
+
+export const metadata: Metadata = {
+  ...metaInfo,
   viewport: {
     width: 'device-width',
     initialScale: 1.0,
+  },
+  openGraph: {
+    ...metaInfo,
+    url: 'https://sridhar-nallasamy.vercel.app/',
+    siteName: metaInfo.title,
+    type: 'website',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/sridhar-nallasamy/sridhar-nallasamy/og-update/assets/png/VgsOgImg.png',
+        width: 1200,
+        height: 627,
+        alt: metaInfo.title,
+      },
+    ],
   },
 };
 
